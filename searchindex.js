1996f7ced9def52e6c1669d69462a20729520109
Dorian Goepp <dorian.goepp@inria.fr>
2015-12-15 10:14:33 +0100

    change theme and welcome message

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to ResiBots&#8217;s documentation!"],titleterms:{document:0,indic:0,resibot:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:[],current:0,heavi:0,index:[],meantim:0,modul:[],offici:0,our:0,page:[],pleas:0,search:[],see:0,thi:0,under:0,websit:0,work:0},titles:["Welcome to ResiBots"],titleterms:{document:[],indic:[],resibot:0,tabl:[],welcom:0}})
