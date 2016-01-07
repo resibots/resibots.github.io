1776941f35a7343baa66c694a57d46e3fbfbf463
Dorian Goepp <dorian.goepp@inria.fr>
2016-01-07 11:17:44 +0100

    add resibot's logo

--- conf.py
+++ conf.py
@@ -148,6 +148,7 @@ html_theme_path = [default_theme_path]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None
+html_logo = "pics/resibots_logo_black_200px.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
