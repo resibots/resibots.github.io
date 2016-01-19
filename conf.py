517c577f37de0240d41ba4c2b3d4e894e73f93f4
Dorian Goepp <dorian.goepp@inria.fr>
2016-01-19 13:44:05 +0100

    add intersphinx links

--- conf.py
+++ conf.py
@@ -308,4 +308,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'https://docs.python.org/': None,
+    'limbo': ('http://www.resibots.eu/limbo/', None),
+}
